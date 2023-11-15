b4df9b5edc91b04cc4397f2a4fb9e71bb4bd805e
emirhangumus <me@emirhangumus.com>
2023-11-15 19:39:05 +0300

    asd

--- src/deneme_backend/src/lib.rs
+++ src/deneme_backend/src/lib.rs
@@ -1,6 +1,5 @@
 mod types;
 
-use ic_cdk_macros::export_candid;
 use types::*;
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
@@ -13,42 +12,145 @@ thread_local! {
 
 #[derive(Default)]
 pub struct MainService {
-    pub users: HashSet<Person>,
-    pub user_count: u32,
+    pub patients: HashSet<Patient>,
+    pub hospitals: HashSet<Hospital>,
 }
 
 
 impl From<BasicServiceStorage> for MainService {
     fn from(stable: BasicServiceStorage) -> MainService {
-        let accounts = stable.users.into_iter().map(|(_, v)| v).collect();
+        let patiens = stable.patients.into_iter().collect();
+        let hospitals = stable.hospitals.into_iter().collect();
 
         MainService {
-            users: accounts,
-            user_count: stable.user_count,
+            patients: patiens,
+            hospitals,
         }
     }
 }
 
 //QUERIES
-#[ic_cdk::query]
-fn get_user_count() -> usize {
-    SERVICE.with(|s| s.borrow().users.len())
-}
-
 #[ic_cdk::update]
-fn add_new_user(name: String, age: u32) -> Result<(), String> {
-    let user: Person = Person { name, age };
-    SERVICE.with(|s| {
-        let mut service = s.borrow_mut();
-        service.users.insert(user);
+fn add_patient(patient: Patient) -> bool {
+    SERVICE.with(|service| {
+        service.borrow_mut().patients.insert(patient);
     });
-    Ok(())
+    true
 }
 
 #[ic_cdk::query]
-fn get_list_users() -> Vec<Person> {
+fn get_patient_by_ssn(ssn: u32) -> Option<Patient> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.clone());
+            }
+        }
+    });
+    patient
+}
 
-    SERVICE.with(|service: &RefCell<MainService>| {
-        service.borrow().users.iter().map(|v: &Person| v.clone()).collect()
-    })
+#[ic_cdk::query]
+fn get_hospital_by_name(hospital_name: String) -> Option<Hospital> {
+    let mut hospital = None;
+    SERVICE.with(|service| {
+        for h in service.borrow().hospitals.iter() {
+            if h.hospital_name == hospital_name {
+                hospital = Some(h.clone());
+            }
+        }
+    });
+    hospital
+}
+
+// #[ic_cdk::query]
+// fn get_patient_general_info(ssn: u32) -> Option<(String, u32, u32, u32)> {
+//     let mut patient = None;
+//     SERVICE.with(|service| {
+//         for p in service.borrow().patients.iter() {
+//             if p.ssn == ssn {
+//                 patient = Some((p.name.clone(), p.age, p.weight, p.height));
+//             }
+//         }
+//     });
+//     patient
+// }
+
+#[ic_cdk::query]
+fn get_patient_blood_type(ssn: u32) -> Option<String> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.blood_type.clone());
+            }
+        }
+    });
+    patient
+}
+
+#[ic_cdk::query]
+fn get_patient_illness_records(ssn: u32) -> Option<Vec<IllnessRecords>> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.illness_records.clone());
+            }
+        }
+    });
+    patient
+}
+
+#[ic_cdk::query]
+fn get_patient_prescription_records(ssn: u32) -> Option<Vec<PrescriptionRecords>> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.prescription_records.clone());
+            }
+        }
+    });
+    patient
+}
+
+#[ic_cdk::query]
+fn get_patient_allergy_records(ssn: u32) -> Option<Vec<AllergyRecords>> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.allergy_records.clone());
+            }
+        }
+    });
+    patient
+}
+
+#[ic_cdk::query]
+fn get_patient_past_treatment_records(ssn: u32) -> Option<Vec<PastTreatmentRecords>> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.past_treatment_records.clone());
+            }
+        }
+    });
+    patient
+}
+
+#[ic_cdk::query]
+fn get_patient_xray_records(ssn: u32) -> Option<Vec<XrayRecords>> {
+    let mut patient = None;
+    SERVICE.with(|service| {
+        for p in service.borrow().patients.iter() {
+            if p.ssn == ssn {
+                patient = Some(p.xray_records.clone());
+            }
+        }
+    });
+    patient
 }
--- src/deneme_backend/src/types.rs
+++ src/deneme_backend/src/types.rs
@@ -1,19 +1,81 @@
-use std::cell::RefCell;
-use std::collections::HashMap;
+use std::{collections::HashSet};
 use ic_cdk::export::{
     candid::{CandidType, Deserialize},
-    Principal,
 };
 
 
 #[derive(Clone, Debug, Default, CandidType, Deserialize)]
 pub struct BasicServiceStorage {
-    pub users: HashMap<Principal, Person>,
-    pub user_count: u32,
+    pub patients: HashSet<Patient>,
+    pub hospitals: HashSet<Hospital>,
 }
 
 #[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
-pub struct Person {
-    pub name: String,
-    pub age: u32,
+pub struct IllnessRecords {
+    pub illness_name: String,
+    pub illness_date: String,
+    pub illness_description: String,
 }
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct PrescriptionRecords {
+    pub prescription_name: String,
+    pub prescription_date: String,
+    pub prescription_description: String,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct AllergyRecords {
+    pub allergy_name: String,
+    pub allergy_date: String,
+    pub allergy_description: String,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct PastTreatmentRecords {
+    pub past_treatment_name: String,
+    pub past_treatment_date: String,
+    pub past_treatment_description: String,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct XrayRecords {
+    pub xray_name: String,
+    pub xray_date: String,
+    pub xray_description: String,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct Patient {
+    pub ssn: u32,
+    pub name: String,
+    pub age: String,
+    pub weight: String,
+    pub height: String,
+    pub blood_type: String,
+    pub illness_records: Vec<IllnessRecords>,
+    pub prescription_records: Vec<PrescriptionRecords>,
+    pub allergy_records: Vec<AllergyRecords>,
+    pub past_treatment_records: Vec<PastTreatmentRecords>,
+    pub xray_records: Vec<XrayRecords>,
+}
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize, PartialEq, Eq, Hash)]
+pub struct Hospital {
+    pub hospital_name: String,
+    pub hospital_address: String,
+}
+
+
+/*
+    boy kilo yaş kan grubu isim vb.
+    Hastalık kayıtları
+    Reçete kayıtları
+    Alerji kayıtları
+    Geçmiş tedavi kayıtları
+    Röntgen kayıtları
+    Tahlil kayıtları
+    Ameliyat kayıtları
+    İlaç kayıtları
+    Aşı kayıtları
+*/
